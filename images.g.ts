02631ccc113b7d754f64b2317ac12c85edc2f66a
Reakter0 <99748424+Ssundee0@users.noreply.github.com>
2022-04-23 12:12:16 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,88 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "DIEING":
+            case "anim1":return [img`
+. . . . . . . f f f f . . . . . 
+. . . . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 2 f . . 
+. . . f 2 2 2 2 2 2 2 2 2 f . . 
+. . . f f f f 2 2 2 2 2 2 f . . 
+. . f 9 6 6 9 f 2 2 2 2 2 f f f 
+. . f 6 6 9 9 9 f 2 2 2 2 f 2 f 
+. . f 9 9 9 9 f 2 2 2 2 2 f 2 f 
+. . . f f f f 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f f f 
+. . . f 2 2 2 2 f 2 2 2 2 f . . 
+. . . f 2 2 2 f . f 2 2 2 f . . 
+. . . f f f f f . f f f f f . . 
+`, img`
+. . . . . . . f f f f . . . . . 
+. . . . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 2 f . . 
+. . . f 2 2 2 2 2 2 2 2 2 f . . 
+. . . f f f f 2 2 2 2 2 2 f . . 
+. . f 9 6 6 9 f 2 2 2 2 2 f f f 
+. . f 6 6 9 9 9 f 2 2 2 2 f 2 f 
+. . f 9 9 9 9 f 2 2 2 2 2 f 2 f 
+. . . f f f f 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f 2 f 
+. . f 2 2 2 2 2 2 2 2 2 2 f f f 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+f 2 2 2 2 2 2 f f f f 2 2 2 2 f 
+. f 2 2 2 f f f . . . f 2 2 f . 
+`, img`
+. . . . . . . f f f f . . . . . 
+. . . . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 2 f . . 
+. . . f 2 2 2 2 2 2 2 2 2 f . . 
+. . . f f f f 2 2 2 2 2 2 f . . 
+. . f 9 6 6 9 f 2 2 2 2 2 f f f 
+. . f 6 6 9 9 9 f 2 2 2 2 f 2 f 
+. . f 9 9 9 9 f 2 2 2 2 2 f 2 f 
+. . . f f f f 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 2 2 2 f f f 
+. . . f 2 2 2 2 f 2 2 2 2 f . . 
+. . . f 2 2 2 f . f 2 2 2 f . . 
+. . . f f f f f . f f f f f . . 
+`, img`
+. . . . . . . f f f f . . . . . 
+. . . . . . f 2 2 2 2 f . . . . 
+. . . . . f 2 2 2 2 2 2 f . . . 
+. . . . f 2 2 2 2 2 2 2 2 f . . 
+. . . f 2 2 2 2 2 2 2 2 2 f . . 
+. . . f f f f 2 2 2 2 2 2 f . . 
+. . f 9 6 6 9 f 2 2 2 2 2 f f f 
+. . f 6 6 9 9 9 f 2 2 2 2 f 2 f 
+. . f 9 9 9 9 f 2 2 2 2 2 f 2 f 
+. . . f f f f 2 2 2 2 2 2 f 2 f 
+. . . f 2 2 2 2 2 2 f 2 2 f 2 f 
+. . . f 2 2 2 2 2 f 2 2 2 f 2 f 
+. . f 2 2 2 2 2 2 2 2 2 2 f f f 
+. f 2 2 2 2 2 2 2 2 2 2 2 2 f . 
+f 2 2 2 2 2 2 f f f f 2 2 2 2 f 
+. f 2 2 2 f f f . . . f 2 2 f . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
